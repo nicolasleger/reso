ad7e96af71dd239d5f3f4dfb528801e699ee3dc0
Antoine Boileau <apem.boileau@gmail.com>
2017-06-22 17:34:39 +0200

    [44_visit_process_improvement_form_test] Happy hacking 😎

--- app/controllers/diagnosis_controller.rb
+++ app/controllers/diagnosis_controller.rb
@@ -5,16 +5,35 @@ class DiagnosisController < ApplicationController
 
   def new
     @visit = Visit.of_advisor(current_user).includes(:company).find params[:visit_id]
+    @diagnosis = Diagnosis.new visit: @visit
   end
 
-  def create; end
+  def create
+    pp params
+    pp diagnosis_params
+    diagnosis = Diagnosis.new visit_id: visit_id_params
+    pp diagnosis
+    # diagnosis = Diagnosis.new @visit
+  end
 
-  def show; end
+  def show
+
+  end
 
   def index; end
 
   def question
     @visit = Visit.of_advisor(current_user).includes(:visitee, :company).find params[:visit_id]
     @question = Question.includes(:assistances, assistances: %i[company user]).find params[:id]
+  end
+
+  private
+
+  def diagnosis_params
+    params.require(:questions)
+  end
+
+  def visit_id_params
+    params.require(:visit_id)
   end
 end
--- app/models/diagnosis.rb
+++ app/models/diagnosis.rb
@@ -3,5 +3,8 @@
 class Diagnosis < ApplicationRecord
   belongs_to :visit
 
+  has_many :diagnosed_needs
+  accepts_nested_attributes_for :diagnosed_needs
+
   validates :visit, presence: true
 end
